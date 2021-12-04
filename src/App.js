0efa3f169c55f5a843f4877713984c9600a0b547
over-geek <kenagy100@gmail.com>
2021-12-04 01:34:48 +0000

    update

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   const fetchWeather = async (e) => {
     e.preventDefault()
     const response = 
-      await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`)
+      await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`)
     setWeather(response.data)
   }
 
